7b46d4a463e80570363db09ac86317685c65403a
danielevicini <daniele.vicini@4ward.it>
2023-03-31 17:09:11 +0200

    revert to simple

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
-$telemetryClient = new \ApplicationInsights\Telemetry_Client();
-$context = $telemetryClient->getContext();
+//$telemetryClient = new \ApplicationInsights\Telemetry_Client();
+//$context = $telemetryClient->getContext();
 
 // Necessary
-$context->setInstrumentationKey('4a0cbc09-9826-4952-a159-a76d4a2e2f3f');
+//$context->setInstrumentationKey('4a0cbc09-9826-4952-a159-a76d4a2e2f3f');
 
 // Start tracking
-$telemetryClient->trackEvent('hello_world_page');
-$telemetryClient->flush();
+//$telemetryClient->trackEvent('hello_world_page');
+//$telemetryClient->flush();
 
 echo "Hello Azure!";
-
-
-
 
 ?>
